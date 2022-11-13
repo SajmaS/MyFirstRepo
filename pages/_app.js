930fe35bd1e00a4730b38962631376470c495d71
EmpereurBiH <maxim.gagnon.mg@gmail.com>
2022-11-13 21:55:50 +0100

    Creating nav bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,26 @@
+import Layout from '../src/components/layout/Layout'
 import '../styles/globals.css'
-import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+import Head from 'next/head'
+import { Montserrat } from '@next/font/google'
+import { config, dom } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
+const montserrat = Montserrat()
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Head>
+  <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+  <style>{dom.css()}</style>
+  </Head>
+  <Layout>
+    <main className={montserrat.className}>
+      <Component {...pageProps} />
+    </main>
+  </Layout>
+  </>
+  )
 }
 
 export default MyApp
